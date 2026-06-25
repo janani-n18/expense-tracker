1e757a7e3295820be43a30af87e6f185ebfc7ec3
janani <jananinatarajan296@gmail.com>
2026-06-25 20:25:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "le@rn.sql",
+  password: process.env.DB_PASSWORD,
   database: "expense_tracker"
 });
 

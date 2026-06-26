a90e314f57c00c8aa44cbd4113bfb716c2f99547
janani <jananinatarajan296@gmail.com>
2026-06-26 11:18:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
@@ -10,10 +11,10 @@ app.use(express.json());
 
 // ================= DB CONNECTION =================
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "le@rn.sql",
-  database: "expense_tracker"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
